eaa3324b9cd9b786f118d0303abce2e4da63af6e
sysner02 <sysner02@sysner02ui-Macmini.local>
2021-03-17 12:32:54 +0900

    00:44:00

--- src/components/Post/components/Footer/index.js
+++ src/components/Post/components/Footer/index.js
@@ -1,21 +1,39 @@
-import React { useState } from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
 import styles from './styles';
 import ADIcon from 'react-native-vector-icons/AntDesign';
 import FontistoIcon from 'react-native-vector-icons/Fontisto';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 
-const Footer = ({ likesCount, caption, postedAt }) => {
+const Footer = ({ likesCount : likesCountProp, caption, postedAt }) => {
 
-    const [isLiked, setIsLike] = useState()
+    const [isLiked, setIsLike] = useState(false);
+
+    const [likesCount, setLikesCount] = useState(0);
+
+    // 좋아요 버튼 누를 때 실행
+    const onLikePressed = () => {
+        const amount = isLiked ? -1 : 1;
+        setLikesCount(likesCount + amount)
+
+        setIsLike(!isLiked);
+    }
+
+    useEffect( () => {
+        setLikesCount(likesCountProp)
+    }, [])
 
     return (
         <View style={styles.container}>
-
             <View style={styles.iconsContainer}>
                 <View style={styles.leftIcons}>
-                    <ADIcon name='hearto' size={25} color={'#545454'} />
+                    <TouchableWithoutFeedback onPress = {onLikePressed}>
+                        {isLiked ?
+                        <ADIcon name='heart' size={25} color={'#e73838'}  />
+                        : <ADIcon name='hearto' size={25} color={'#545454'}  />
+                    }
+                    </TouchableWithoutFeedback>
                     <FontistoIcon name="comment" size={23} color={'#545454'} />
                     <Ionicons name='paper-plane-outline' size={25} color={'#545454'} />
                 </View>
